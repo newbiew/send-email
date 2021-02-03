b4991604bc3904669a880c180c206cd50922ada7
newbiew83 <newbiew@newbiews-iMac.local>
2021-02-03 10:46:25 +0800

    update home route

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,6 @@
 
 
 
-Route::get('/import', 'ImportController@getImport')->name('import');
+Route::get('/', 'ImportController@getImport')->name('import');
 Route::post('/import_parse', 'ImportController@parseImport')->name('import_parse');
 Route::post('/import_process', 'ImportController@processImport')->name('import_process');
